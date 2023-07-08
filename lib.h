d04e9da3101a22bcac3d95236225464d3c95e2b8
Nelson1Aguiar <132019512+Nelson1Aguiar@users.noreply.github.com>
2023-07-07 21:40:50 -0300

    Update lib.h

--- lib.h
+++ lib.h
@@ -5,6 +5,9 @@
 #include <time.h>
 #include <string.h>
 #define TAM 2
+#define pai(i) ((i-1)/2)
+#define esq(i) (2*i+1)
+#define dir(i) (2*i+2)
 
 typedef struct pedido{
     char comida[30];
@@ -46,6 +49,12 @@ typedef struct No{
     struct No* proximo;
 }No;
 
+typedef struct{
+    cliente *v;
+    int tam;
+    int n;
+}heapMin;
+
 void menu(void);
 heap* cria_heap(cliente c);
 void troca_filhos (heap* a);
@@ -53,6 +62,7 @@ void troca_filhos (heap* a);
 heap* uniao(heap * h1, heap* h2);
 heap* remover(heap *h);
 heap* uniao_heaps(heap* h1, heap* h2);
+
 
 void imprime(heap *h,caixa *c);
 
@@ -64,3 +74,15 @@ void listaCaixas(No* no);
 int buscarCaixaMenosCliente(No* cabeca,bool p);
 caixa* buscarUmCaixa(No* cabeca,int c);
 void removerCaixa(No** cabeca, int num);
+void menu();
+
+heapMin* criarHeapSimples(int maximo);
+void heap_insere(heapMin *h,cliente c);
+void sobe(heapMin *h,int pos);
+void trocar(heapMin *h,int posicaoPai,int pos);
+void imprimir(heapMin h);
+cliente* removerSimples(heapMin *h);
+void descer(heapMin *h,int i);
+void letreiro(heap *h,caixa *c);
+bool temCliente(No *no,int num);
+void notaFiscal(No *no,int caixa,int pagamento);
