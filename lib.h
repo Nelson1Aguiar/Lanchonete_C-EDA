50dc630d416f84892c599de84e0b15c368278790
Nelson1Aguiar <132019512+Nelson1Aguiar@users.noreply.github.com>
2023-07-08 14:52:47 -0300

    Update lib.h

--- lib.h
+++ lib.h
@@ -27,7 +27,6 @@ typedef struct cliente{
 typedef struct no{
     cliente clientes;
     int s;
-
     struct no *esq;
     struct no *dir;
 }heap;
@@ -53,29 +52,20 @@ typedef struct{
 void menu(void);
 heap* cria_heap(cliente c);
 void troca_filhos (heap* a);
-
 heap* uniao(heap * h1, heap* h2);
 heap* remover(heap *h);
 heap* uniao_heaps(heap* h1, heap* h2);
-
-
 void imprime(heap *h,caixa *c);
-
-
-
 void inserirNoComeco(No** cabeca, caixa *c);
-
 void listaCaixas(No* no);
 int buscarCaixaMenosCliente(No* cabeca,bool p);
 caixa* buscarUmCaixa(No* cabeca,int c);
 void removerCaixa(No** cabeca, int num);
 void menu();
-
 heapMin* criarHeapSimples(int maximo);
 void inserirHeapSimples(heapMin* heap, cliente c);
 void subir(heapMin* heap, int i);
 void troca(cliente *a, cliente *b);
-void imprimir(heapMin h);
 void removerSimples(heapMin *h);
 void descer(heapMin* heap, int i);
 void letreiro(heap *h,caixa *c);
